f295b5fc2e713f85a1deef64592088cab7c8c6c0
Andsirpa <andsirpa94@gmail.com>
2024-01-10 08:32:35 +0100

    add user's info and price

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+const distance = parseInt(prompt('quanti km vuoi percorrere?'))
+console.log(distance)
+
+const age = parseInt(prompt('quanti anni hai?'))
+console.log(age)
+
+const price = distance * 0.21
+console.log(price)
+
